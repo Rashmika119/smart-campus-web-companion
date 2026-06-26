6f8604a6ed1bbc2089fb79224f9d901bdb4d6b15
Rashmika Nethsarani <RashmikaN@fortude.co>
2026-06-26 20:43:16 +0530

    fix issue:resolve minor issues

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/smart-campus-web-companion/dashboard',
+  base: '/smart-campus-web-companion/',
   plugins: [react()],
 })
